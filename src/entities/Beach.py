775803ed6b0048bbb49e50c03081a9d71c785b40
Javi Plaza <javierplazasisques@gmail.com>
2021-09-02 15:20:58 +0200

    Updated Town class

---
+++ src/entities/Beach.py
@@ -0,0 +1,8 @@
+from src.entities.BeachInformation import *
+
+class Beach():
+
+    def __init__(self, id, beach_id, beach_information):
+        self.id = id
+        self.beach_id = beach_id
+        self.beach_information = beach_information
---
+++ src/entities/Town.py
@@ -0,0 +1,5 @@
+class Town:
+
+    def __init__(self, cod, name):
+        self.cod = cod
+        self.name = name
