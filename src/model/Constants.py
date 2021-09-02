2594704d491fb545615ea76cdd3aafa8be4ab2fe
Javi Plaza <javierplazasisques@gmail.com>
2021-09-02 15:56:04 +0200

    Updated Constants.py

--- src/model/Constants.py
+++ src/model/Constants.py
@@ -7,4 +7,7 @@ class Constants():
     FILE_TYPE_JSON = ".json"
     FILE_TYPE_TXT = ".txt"
     FILE_TYPE_CSV = ".csv"
+    FILE_TYPE_XLSX = ".xlsx"
 
+    URL_BEACHES_FILE = "http://www.aemet.es/documentos/es/eltiempo/prediccion/playas/Playas_codigos.csv"
+    URL_TOWNS_FILE = "https://www.ine.es/daco/daco42/codmun/codmun20/20codmun.xlsx"
