dbea9c52f6390891d28acc9e064779c49267838d
Javi Plaza <javierplazasisques@gmail.com>
2021-09-02 16:25:24 +0200

    Updated Beach.py

--- src/entities/Beach.py
+++ src/entities/Beach.py
@@ -2,6 +2,7 @@ from src.entities.BeachInformation import *
 
 class Beach():
 
-    def __init__(self, beach_information, beach_code):
+    def __init__(self, id, beach_information, beach_code):
+        self.id = id
         self.beach_information = beach_information
         self.beach_code = beach_code
