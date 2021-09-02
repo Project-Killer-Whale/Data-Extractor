b8f1f4cd366f22967a1eec680989401295a617e5
Javi Plaza <javierplazasisques@gmail.com>
2021-09-02 16:02:06 +0200

    Updated Main.py

--- Main.py
+++ Main.py
@@ -1,3 +1,9 @@
+from typing import Container
+from src.persistance.JSONFormatter import *  
+from src.persistance.FileManager import *
+from src.model.Downloader import *
+from src.model.Constants import * 
+
 def main():
     download_files()
     read_files()
@@ -5,7 +11,8 @@ def main():
     save_data()
 
 def download_files():
-    pass
+    Downloader.download(Constants.URL_BEACHES_FILE, Constants.DATA_PATH, "Beach_codes", Constants.FILE_TYPE_CSV)
+    Downloader.download(Constants.URL_TOWNS_FILE, Constants.DATA_PATH, "Town_codes", Constants.FILE_TYPE_XLSX)
 
 def read_files():
     pass
--- src/model/Constants.py
+++ src/model/Constants.py
@@ -2,7 +2,7 @@ class Constants():
     
     PATH = "./"
     OU_PATH = PATH + "output/"
-    IN_PATH = PATH + "data/"
+    DATA_PATH = PATH + "data/"
 
     FILE_TYPE_JSON = ".json"
     FILE_TYPE_TXT = ".txt"
