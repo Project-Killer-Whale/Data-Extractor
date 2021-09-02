c308137a5ca6bd3e4f6a12fd87afd3d70c57842c
Javi Plaza <javierplazasisques@gmail.com>
2021-09-02 15:43:03 +0200

    Updated Main.py

--- Main.py
+++ Main.py
@@ -1,4 +1,19 @@
 def main():
+    download_files()
+    read_files()
+    process_files()
+    save_data()
+
+def download_files():
+    pass
+
+def read_files():
+    pass
+
+def process_files():
+    pass
+
+def save_data():
     pass
 
 main()
