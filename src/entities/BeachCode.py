20330a445a9d2474ec4f915ba21857bf086086a1
Javi Plaza <javierplazasisques@gmail.com>
2021-09-02 16:23:22 +0200

    Created BeachCode.py

---
+++ src/entities/BeachCode.py
@@ -0,0 +1,11 @@
+class BeachCode():
+
+    def __init__(self, id, nombre, id_municipio, municipio, id_provincia, provincia, latitud, longitud):
+        self.id = id
+        self.nombre = nombre
+        self.id_municipio = id_municipio
+        self.municipio = municipio
+        self.id_provincia = id_provincia
+        self.provincia = provincia
+        self.latitud = latitud
+        self.longitud = longitud
