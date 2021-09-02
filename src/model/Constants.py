ce91a9fe5dd24e63a2786e1641bcdc0519e915a5
Javi Plaza <javierplazasisques@gmail.com>
2021-09-02 16:09:39 +0200

    Updated Main.py

--- src/model/Constants.py
+++ src/model/Constants.py
@@ -12,3 +12,6 @@ class Constants():
     URL_BEACHES_FILE = "http://www.aemet.es/documentos/es/eltiempo/prediccion/playas/Playas_codigos.csv"
     URL_TOWNS_FILE = "https://www.ine.es/daco/daco42/codmun/codmun20/20codmun.xlsx"
     URL_BEACHES_INFORMATION = "https://sig.mapama.gob.es/WebServices/clientews/Guia-Playas/default.aspx?origen=1010&nombre=PLAYAS&claves=DGC.PLAYAS.PLY_CO_PLAYA&valores="
+
+    FILE_NAME_BEACHES = "Beach_codes" 
+    FILE_NAME_TOWNS = "Town_codes"
